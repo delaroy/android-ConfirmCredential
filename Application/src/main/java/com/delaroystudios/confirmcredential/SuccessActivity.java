519abb69b43177888b631d692fd0741fe301a917
Delaroy Studios <bamoyk@yahoo.com>
2018-01-28 23:26:51 +0100

    Initial commit

---
+++ Application/src/main/java/com/delaroystudios/confirmcredential/SuccessActivity.java
@@ -0,0 +1,20 @@
+package com.delaroystudios.confirmcredential;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.confirmcredential.R;
+
+/**
+ * Created by delaroy on 1/28/18.
+ */
+
+public class SuccessActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.success_layout);
+    }
+}
